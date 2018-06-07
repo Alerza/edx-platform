e3da0b5ebed810623c32e8352fa1c6dc46169935
irfanuddinahmad <irfan.ahmad@arbisoft.com>
2018-06-07 15:57:29 +0500

    incorporated manual verification

--- openedx/core/djangoapps/user_api/verification_api/views.py
+++ openedx/core/djangoapps/user_api/verification_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.generics import RetrieveAPIView
 from rest_framework_oauth.authentication import OAuth2Authentication
 
-from lms.djangoapps.verify_student.models import SoftwareSecurePhotoVerification, SSOVerification
+from lms.djangoapps.verify_student.models import SoftwareSecurePhotoVerification, SSOVerification, ManualVerification
 from lms.djangoapps.verify_student.utils import most_recent_verification
 from openedx.core.djangoapps.user_api.serializers import (
     SoftwareSecurePhotoVerificationSerializer, SSOVerificationSerializer,
@@ -34,9 +34,15 @@ class IDVerificationStatusView(RetrieveAPIView):
         photo_verifications = SoftwareSecurePhotoVerification.objects.filter(
             user__username=username).order_by('-updated_at')
         sso_verifications = SSOVerification.objects.filter(user__username=username).order_by('-updated_at')
+        manual_verifications = ManualVerification.objects.filter(user__username=username).order_by('-updated_at')
 
-        if photo_verifications or sso_verifications:
-            verification = most_recent_verification(photo_verifications, sso_verifications, 'updated_at')
+        if photo_verifications or sso_verifications or manual_verifications:
+            verification = most_recent_verification(
+                photo_verifications,
+                sso_verifications,
+                manual_verifications,
+                'updated_at'
+            )
             self.check_object_permissions(self.request, verification)
             return verification
 
